51323f19325f9886aa642c5eb3d43ad8d8ddc531
poropo97 <poropo97@gmail.com>
2021-02-12 14:02:19 +0100

    require laravel 8

--- src/routes.php
+++ src/routes.php
@@ -1,4 +1,20 @@
  <?php
-    Route::get('testeo', function(){
-        return 'Hello from the testeo form package, second version for testing';
-    });
+   /*
+  |--------------------------------------------------------------------------
+  | RUTAS DE ADMIN
+  |--------------------------------------------------------------------------
+  |
+  */
+
+  Route::group(['middleware' => ['auth','onlyAdmin']], function()
+  {
+    // DEFAULT
+    Route::get('/admin',                        'AdminController@dashboard');
+    Route::get('/admin/{modelName}',            'AdminController@listModel');
+    Route::get('/admin/{modelName}/add',        'AdminController@addModel');
+    Route::post('/admin/{modelName}/add',       'AdminController@addDataModel');
+    Route::get('/admin/{modelName}/edit/{id}',  'AdminController@editModelExists');
+    Route::any('/admin/{modelName}/remove/{id}','AdminController@deleteDataModel');
+    // logout
+    Route::any('/admin/logout',                 'Auth\AuthController@logout');
+  });
